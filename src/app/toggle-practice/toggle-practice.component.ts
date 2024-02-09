28eb39d197773168255a9bab17373a36783e8787
deepak-singh-moglix <deepakshivam1998@gmail.com>
2024-02-09 22:59:16 +0530

    added

---
+++ src/app/toggle-practice/toggle-practice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-practice',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-practice.component.html',
+  styleUrl: './toggle-practice.component.css'
+})
+export class TogglePracticeComponent {
+
+}
